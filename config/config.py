dac693027a1af017822ac9e4fc0aa6414c9dce2a
Romanenko Vladimir <bonyshd@gmail.com>
2026-07-06 09:48:30 +0300

    fix

--- config/config.py
+++ config/config.py
@@ -4,11 +4,5 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-# Привет
 TOKEN = os.getenv("TOKEN")
 
-# ghgkhfk bmobmoimbxfopxn
-OR_TOKEN = os.getenv("OR_TOKEN")
-# ндфл
-DB_NAME = os.getenv("DB_NAME")
-# я человек паук
